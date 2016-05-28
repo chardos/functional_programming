4b0265d020f07316940087ab4f8ef69564f2b331
chardos <chardos@gmail.com>
2016-05-28 22:06:07 +1000

    Reduce working!

--- arrays.js
+++ arrays.js
@@ -12,6 +12,14 @@ Array.prototype.map = function(fn, newArr){
   return newArr;
 }
 
+Array.prototype.reduce = function(fn, remainingArr, currValue){
+  if(currValue) currValue = fn( currValue, this[1] );
+  if(!currValue) currValue = fn( this[0], this[1] );
+  var remainingArr = this.slice(1);
+  if(remainingArr.length > 1) return remainingArr.reduce(fn, remainingArr, currValue)
+  return currValue;
+}
+
 Array.prototype.allSame = function(comparedValue){
   if(!comparedValue) return this.allSame(this[0])
   if(this[0] !== comparedValue) return false;
--- current.js
+++ current.js
@@ -1,7 +1,7 @@
 Array.prototype.reduce = function(fn, remainingArr, currValue){
-  currValue = currValue || 0;
-  currValue = fn(currValue, this[0]);
+  if(currValue) currValue = fn( currValue, this[1] );
+  if(!currValue) currValue = fn( this[0], this[1] );
   var remainingArr = this.slice(1);
-  if(remainingArr.length > 0) return remainingArr.reduce(fn, remainingArr, currValue)
+  if(remainingArr.length > 1) return remainingArr.reduce(fn, remainingArr, currValue)
   return currValue;
 }
